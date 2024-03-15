531123276b918cd83247f5628168e34bc5a0433e
Yui <dev@sleepyyui.com>
2024-03-15 10:12:51 +0100

    add automounting

--- rip.sh
+++ rip.sh
@@ -2,6 +2,7 @@
 
 # get dvd file path
 dvd_file_path=$1
+current_timestamp=$(date +%s)
 
 # if dvd file path is empty, search for dvdrom path
 if [ -z $dvd_file_path ]; then
@@ -11,10 +12,26 @@ if [ -z $dvd_file_path ]; then
     echo "DVD file path is $dvd_file_path"
 fi
 
+# check if dvd is mounted
+mount_point=$(df -h | grep $dvd_file_path | awk '{print $6}')
+if [ -z $mount_point ]; then
+    echo "DVD is not mounted"
+    echo "Mounting DVD"
+    mount_point="/media/dvd/$current_timestamp"
+    # check if mount point exists
+    if [ -d $mount_point ]; then
+        echo "Mount point $mount_point exists"
+    else
+        echo "Creating mount point $mount_point"
+        sudo mkdir -p $mount_point
+    fi
+    sudo mkdir -p $mount_point
+    sudo mount $dvd_file_path $mount_point
+fi
+echo "DVD is mounted at $mount_point"
+
 # get disk info
 ./venv/bin/python main.py --scan -i $dvd_file_path
-
-current_timestamp=$(date +%s)
 
 # rip dvd
 ./venv/bin/python main.py -i $dvd_file_path -o ./outs/$current_timestamp
